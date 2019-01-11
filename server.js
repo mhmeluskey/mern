6d9ddaddf8f70290dc501769de4d080442a76904
Megan <mhmeluskey@gmail.com>
2019-01-11 18:19:32 -0500

    users database

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/co2food");
+const db = require("./models");
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("/allusers", function(req, res) {
+  console.log("All users route was hit");
+  db.User.find({})
+    .sort({ date: -1 })
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
