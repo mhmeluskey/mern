c5b777110a3f204ba1b4254e55b348d5e1025657
Megan <mhmeluskey@gmail.com>
2019-01-17 10:23:41 -0500

    footprint card

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>{" "}
+        </header>
         <Form />
         <Foot />
       </div>
--- client/src/components/Foot.js
+++ client/src/components/Foot.js
@@ -1,22 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Foot.css";
 import footprint from "./Footprint.png";
 
-class Foot extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="card">
-          <button>Add</button>
-          <button>&times; </button>
-          <div className="img-container">
-            <img src={footprint} alt="footprint" height="140px" />
-          </div>
+function Foot(props) {
+  return (
+    <div className="wrapper">
+      <div className="card">
+        <button>Add</button>
+        <button>&times; </button>
+        <div className="img-container">
+          <img src={footprint} alt="footprint" />
+        </div>{" "}
+        <div className="results">
+          <p className="result__food">
+            Food:
+            {/* {co2food.food} */}
+            <br /> Co2 in Kilos:
+            {/* {co2food.co2} */}
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Foot;
