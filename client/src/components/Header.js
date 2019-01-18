5ddaf230a3adb7f397b58eec67b4b41905c3ad17
Megan <mhmeluskey@gmail.com>
2019-01-17 19:10:12 -0500

    links to different pages

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./Header.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export function Header(props) {
   return (
     <div className="nav">
-      <a className="title" href="#">
+      <Link
+        to="/"
+        onClick={() => props.handlePageChange("Home")}
+        className="navbar-brand"
+      >
         Co2 of Your Plate
-      </a>
-      <div className="links">Home | Meals</div>
+      </Link>
+      <div className="links">
+        <Link to="/" onClick={() => props.handlePageChange("Home")}>
+          Home |
+        </Link>
+
+        <Link to="/meals" onClick={() => props.handlePageChange("Meals")}>
+          Meals
+        </Link>
+      </div>
     </div>
   );
 }
